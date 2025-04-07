97d5ea9b9ae220b4cd40a80d3be3d441ffbebaea
priyanka kakani <kakanipriyanka402@gmail.com>
2025-04-07 15:07:01 -0400

    commiting all that addition sample project

---
+++ src/com/example/addition/Addition.java
@@ -0,0 +1,17 @@
+package com.example.addition;
+
+public class Addition {
+
+	public static void main(String[] args) {
+		
+		int a = 2;
+		int b = 2;
+		int sum = a+b;
+		
+		System.out.println("sum:" + sum);
+		
+
+
+	}
+
+}
